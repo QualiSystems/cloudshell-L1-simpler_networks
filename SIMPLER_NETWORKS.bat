e574406fdca4a3e6eb747ddfad742241e68527eb
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2017-11-10 17:35:10 +0200

    Fixed bat

--- SIMPLER_NETWORKS.bat
+++ SIMPLER_NETWORKS.bat
@@ -5,15 +5,11 @@ set DRIVERS_FOLDER=%SERVER_FOLDER%\Drivers
 set DRIVER_NAME=%~n0
 set LOGS_PATH=%SERVER_FOLDER%\Logs\%DRIVER_NAME%
 set DRIVER_ENV=%DRIVERS_FOLDER%\%DRIVER_NAME%
-set PYTHON=%DRIVER_ENV%\Scripts\python
-set EXE=%PYTHON% %DRIVER_ENV%\main.py
+set PYTHON="%DRIVER_ENV%\Scripts\python"
+set EXE=%PYTHON% "%DRIVER_ENV%\main.py"
 set port=%1
 if not defined port set port=4000
 echo Starting driver %DRIVER_NAME%
 echo Log Path %LOGS_PATH%
 %EXE% %port% %LOGS_PATH%
-::echo %PYTHONPATH%
-::echo %DRIVER_ENV%\cloudshell
-::echo %EXE%
-::%PYTHON%
 endlocal
